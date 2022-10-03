75916cd160ce6153f45f32329c9cbe5c6916ea77
Alero <francessmaku@gmail.com>
2022-10-02 21:20:26 -0400

    testing npm

---
+++ my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-lib',
+  template: ` <p>Code Shots With Profanis</p> `,
+  styles: [],
+})
+export class MyLibComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
